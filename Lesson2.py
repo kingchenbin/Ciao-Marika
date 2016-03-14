1083a6aef0aaa3c75fc8e13e207e0dd4621e3b5d
beingchen <kingcb@qq.com>
2016-03-14 21:27:21 +0800

    refine lesson1,2,3

--- Lesson2.py
+++ Lesson2.py
@@ -1,2 +1,9 @@
-print "We have %r team members, and our slogan is %s" %(8, "'Try Everything!'")
+# A comment, this is so you can read your program later.
+#Anything after # is ignored by python
 
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run"
+
+print "This will run."
