590075ea6a20330d98ab24bc8f48d1759f35fe1b
beingchen <kingcb@qq.com>
2016-03-14 22:04:28 +0800

    update

---
+++ Lesson5.py
@@ -0,0 +1,17 @@
+my_name = 'Being Chen'
+my_age = 26 # that's true
+my_height = 193 # centimeters
+my_weight = 77 # kilograms
+my_eyes = 'Green and Blue' # I am a persian cat
+my_teeth = 'White'
+my_hair = 'Ginger'
+print "Let's talk about %s." % my_name
+print "He is about %d centimeters tall." % my_height
+print "He is about %d kilograms heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# This line is tricky, try to get it exactly right.
+print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight,
+	my_age + my_height + my_weight)
