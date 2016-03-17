35440f8209eac73e5f092c5d0532ce2041c39453
beingchen <kingcb@qq.com>
2016-03-17 22:25:28 +0800

    update

---
+++ Lesson16.py
@@ -0,0 +1,30 @@
+from sys import argv
+
+script, filename = argv
+
+print "We are going to erase %r." % filename
+print "If you do not want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
+
+raw_input('?')
+
+print "Opening the file..."
+target = open(filename, 'r+')
+
+print "Truncating the file. Goodbye!"
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("Line 1: ")
+line2 = raw_input("Line 2: ")
+line3 = raw_input("Line 3: ")
+
+print "I'm going to write these to the file."
+
+target.write(line1+'\n')
+target.write(line2+'\n')
+target.write(line3+'\n')
+
+print "And finally, we close it."
+target.close()
