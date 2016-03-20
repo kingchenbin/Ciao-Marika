9730e5a687b35dc2a6b05ff292b955bd794a958a
beingchen <kingcb@qq.com>
2016-03-20 22:52:57 +0800

    update

---
+++ Lesson25_provoke.py
@@ -0,0 +1,22 @@
+import Lesson25
+
+sentence = "We are a very competive team"
+words = Lesson25.break_words(sentence)
+
+print words
+
+print Lesson25.sort_words(words)
+
+Lesson25.print_first_word(words)
+
+Lesson25.print_last_word(words)
+
+sentence = "I am waiting for you to break up with him"
+
+print Lesson25.break_words(sentence)
+
+print Lesson25.sort_sentence(sentence)
+
+Lesson25.print_first_and_last(sentence)
+
+Lesson25.print_first_and_last_sorted(sentence)
