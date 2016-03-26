6db4a163c8d73b7208997910fbe446ea9de484e1
beingchen <kingcb@qq.com>
2016-03-26 21:38:25 +0800

    update

--- Lesson37_advanced.py
+++ Lesson37_advanced.py
@@ -12,3 +12,15 @@ x = 520
 print x
 showGlobal()
 print x
+
+print "\n2. assert\n"
+
+def greater(a, b):
+	if a >= b:
+		return True
+	elif a < b:
+		return False
+	else:
+		assert(0)
+
+print "Does %r greater that %r? %r" %(15, 1.75, greater(15, 1.75))
