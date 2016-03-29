13924fe2bfd334a36e3c45c43439620950a8c262
beingchen <kingcb@qq.com>
2016-03-29 21:57:01 +0800

    update

--- Lesson37_operator.py
+++ Lesson37_operator.py
@@ -1,6 +1,58 @@
+from __future__ import division
 
 print "Today is " + "%s" % "Monday."
 
-print "%d/03/%d" % (1008*2, 31-3)
+print "%d/%02d/%d" % (1008*2, 300/100, 31-3)
 
-print ""
+print "Do you know how to compute 6^6? It is %d." % 6**6
+
+print "Integer division 3.5/2 =", 3.5//2
+
+print "101 % 24 =", 101 % 24
+
+print "< > <= >= == !=/<>"
+
+#list
+zoo = ['pig','tiger','dog']
+print zoo
+#tuple
+country = ('bj', ['sh','gd'])
+print country
+#dictionary
+dict = {'name':'Pei', 'age':'26'}
+print dict
+
+def minus(f):
+	print 'minus'
+	f()
+
+def plus(f):
+	print 'plus'
+	f()
+
+def test(a):
+	if a > 3:
+		print "pass"
+		return plus
+	else:
+		print "fail"
+		return minus
+
+@test(5)
+def xxx():
+	print 'ok'
+
+@minus
+def xxx():
+	print 'ok'
+
+#@dec2  
+#@dec1  
+#def func(arg1, arg2, ...):  
+#    pass 
+
+#def func(arg1, arg2, ...):  
+#    pass  
+#func = dec2(dec1(func))   
+
+print "OK, Lesson37 finished!";
