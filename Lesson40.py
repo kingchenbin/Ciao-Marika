7fa9e63ae2c50ce086b0e0d8387ae5acdd47ae6c
beingchen <kingcb@qq.com>
2016-04-06 21:10:59 +0800

    update after a few days' absence

--- Lesson40.py
+++ Lesson40.py
@@ -9,3 +9,23 @@ class MyStuff(object):
 thing = MyStuff("you and me.")
 thing.apple()
 print thing.tangerine
+
+class Song(object):
+	"""docstring for Song"""
+	def __init__(self, arg):
+		self.lyrics = arg
+
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print line
+
+happy_bday = Song(["Happy birthday to you",
+	"I don't want to get sued",
+	"So I'll stop right here"])
+
+bulls_on_parade = Song(["They really around the family",
+	"With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
