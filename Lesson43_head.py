9c008cfce74794fea68eec3567baffdb5d0a4ff9
beingchen <kingcb@qq.com>
2016-04-15 22:22:34 +0800

    update

--- Lesson43_head.py
+++ Lesson43_head.py
@@ -32,7 +32,28 @@ class CentralCorridor(Scene):
 		print "The Gothons of Planet Percal #25 have invaded your ship and destroyed"
 		print "your entire crew. You are the last surviving member and your last"
 		print "mission is to get the neutron destruct bomb from the Weapon Armory,"
-		pass
+		print "put it in the bridge, and blow the ship up after getting into an "
+		print "escape pod."
+		print "\n"
+		print "You're running down the central corridor to the Weapon Armory when"
+		print "a Gothon jumps out, red scaly skin, dark grimt teeth, and evil clown costume"
+		print "flowing around his hate filled body. He's blocking the door to the"
+		print "Armory and about to pull a weapon to blast you."
+
+		action = raw_input("> ")
+
+		if action == "shoot!":
+			print "Quick on the draw you yank out your blaster and fire it at the Gothon."
+			print "His clown costume is flowing and moving around his body, which throws"
+			print "off your aim. Your laser hits costume but misses him entirely. This"
+			print "completely ruins his brand new costume his mother bought him, which"
+			print "makes him fly into a rage and blast you repeatedly in the face until"
+			print "you are dead. Then he eats you."
+			return 'death'
+		
+		if action == "dodge!":
+			print "Like a world class boxer you dodge, weave, slip and silde right"
+			pass
 
 class LaserWeaponArmory(Scene):
 	def enter(self):
